e10123c6172ae8296b2f872fafddd057ba0254f3
alialphasquad <muhammadali.alphasquad@gmail.com>
2022-10-24 15:16:08 +0500

    Latest

--- route/attendanceRoute.ts
+++ route/attendanceRoute.ts
@@ -52,8 +52,6 @@ const router = express.Router();
  *         application/json:
  *           schema:
  *             $ref: '#/components/schemas/Attendance'
- *     security:
- *       - myAuth: []
  *     responses:
  *       200:
  *         description: Add Attendance
@@ -80,8 +78,6 @@ router.route("/addAttendance").post(addAttendance);
  *           required: true
  *           schema:
  *             type: integer
- *       security:
- *         - myAuth: []
  *       responses:
  *         200:
  *           description: Success.
